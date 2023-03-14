7863e45cf2dd3bddf8c1531bcc5c99309d891418
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-03-15 00:23:30 +0700

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
-global.owner = ['6281220670449'] // no own
-global.author = 'KurrXd' // nama author 
-global.packname = 'xd bot whatsapp' // nama pack sticker
-global.namabot = 'XD BOTZ' // nama bot mu
-global.group = 'https://bit.ly/3ZI51Qk' // grup mu
-global.pic = 'https://telegra.ph/file/b96926809d93d9f045777.jpg' // logo bot
+global.owner = ['6285923463470'] // no own
+global.author = 'AnggaKz' // nama author 
+global.packname = 'Lisabotz-MD whatsapp' // nama pack sticker
+global.namabot = 'Lisabotz-MD' // nama bot mu
+global.group = 'https://chat.whatsapp.com/Ly28IB8Fel4JbPS0qCUy2p' // grup mu
+global.pic = 'https://telegra.ph/file/3a0e3e7784ddfcf0c5afc.jpg' // logo bot
 
 
 
